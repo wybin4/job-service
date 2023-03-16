2dd1b923fc275e2707d1e522b2e40c241ee19979
ajdivotf <eckyl@bk.ru>
2023-03-16 19:15:44 +0300

    try to fix host

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,10 +39,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path(),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
