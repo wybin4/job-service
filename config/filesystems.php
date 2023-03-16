b9852bcda4cffdc35223edf7ee8d818e5d8d77ab
ajdivotf <eckyl@bk.ru>
2023-03-16 19:23:45 +0300

    tryyyy

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,9 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'throw' => false,
         ],
 
         's3' => [
