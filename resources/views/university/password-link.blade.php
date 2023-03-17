a51645df9f5184d94f26b31a99bdc5dbb3f1f1e5
ajdivotf <eckyl@bk.ru>
2023-03-17 16:06:13 +0300

    fix mailable

--- resources/views/university/password-link.blade.php
+++ resources/views/university/password-link.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-<p style="text-align:center">Чтобы войти на сайт, пройдите по ссылке ниже</p>
+<p style="text-align:center">Чтобы войти на сайт и начать регистрировать студентов, пройдите по ссылке ниже</p>
 @component('mail::button', ['url' => $url])
 Нажмите сюда
 @endcomponent
