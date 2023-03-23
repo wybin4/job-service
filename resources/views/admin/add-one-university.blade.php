54b7b93068eba609d92de689771fff12094b91d0
ajdivotf <eckyl@bk.ru>
2023-03-23 16:51:19 +0300

    fix mistake in uni and employer registration

--- resources/views/admin/add-one-university.blade.php
+++ resources/views/admin/add-one-university.blade.php
@@ -8,7 +8,7 @@
 <x-admin-layout>
 	<div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0 bg-gray-100">
 		<div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-			<h2 class="header-text text-center" style="margin-bottom:30px;">Зарегестрировать</h2>
+			<h2 class="header-text text-center" style="margin-bottom:30px;">Зарегистрировать</h2>
 			@if ($errors->any())
 			@foreach ($errors->all() as $error)
 			<script>
