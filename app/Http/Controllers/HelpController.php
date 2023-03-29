13b2a7801de8e45bd83e4828b388eac31bf082f1
ajdivotf <eckyl@bk.ru>
2023-03-29 09:50:55 +0300

    fix HelpController

--- app/Http/Controllers/HelpController.php
+++ app/Http/Controllers/HelpController.php
@@ -131,13 +131,14 @@ class HelpController extends Controller
 		////
 		///
 		//все офферы студентам за период
-		$ungrouped_uni_offers_count = Interaction::join('students', 'students.id', '=', 'interactions.student_id')
+		/*$ungrouped_uni_offers_count = Interaction::whereBetween('interactions.created_at', [$start . ' 00:00:00', $end . ' 23:59:59'])
+		->join('students', 'students.id', '=', 'interactions.student_id')
 		->where('type', 1)
 			->groupBy('students.id')
 			->select('university_id', 'students.id', DB::raw('count(*) as total'))
 			->get()
-			->toArray();
-		dd($ungrouped_uni_offers_count, $start, $end);
+			->toArray();*/
+		dd(Interaction::whereBetween('interactions.created_at', [$start . ' 00:00:00', $end . ' 23:59:59']));
 		//группируем офферы по вузам
 		$grouped_uni_offers_count = $algo->_group_by($ungrouped_uni_offers_count, "university_id");
 		//////
