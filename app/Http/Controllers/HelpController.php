4c70c37b75348c34e929e290aa780d5b8762ca4a
ajdivotf <eckyl@bk.ru>
2023-03-29 09:55:02 +0300

    fix HelpController

--- app/Http/Controllers/HelpController.php
+++ app/Http/Controllers/HelpController.php
@@ -138,7 +138,7 @@ class HelpController extends Controller
 			->select('university_id', 'students.id', DB::raw('count(*) as total'))
 			->get()
 			->toArray();*/
-		dd(Interaction::whereDate('interactions.created_at', '<=', $start . ' 00:00:00')->get());
+		dd(Interaction::whereDate('interactions.created_at', '>=', $end . ' 00:00:00')->get());
 		//группируем офферы по вузам
 		$grouped_uni_offers_count = $algo->_group_by($ungrouped_uni_offers_count, "university_id");
 		//////
